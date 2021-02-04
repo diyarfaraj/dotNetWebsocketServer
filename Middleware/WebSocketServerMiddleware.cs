f4470763c4731985e5092c4c76b4bad338e74a49
diyarfaraj <diyar.faraj@gmail.com>
2021-02-04 22:04:47 +0100

    added targeted messaging

--- Middleware/WebSocketServerMiddleware.cs
+++ Middleware/WebSocketServerMiddleware.cs
@@ -6,6 +6,7 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WebSocketServer.Middleware
 {
@@ -35,11 +36,17 @@ namespace WebSocketServer.Middleware
                     if (result.MessageType == WebSocketMessageType.Text)
                     {
                         Console.WriteLine("message recieved");
+                        Console.WriteLine($"Message: {Encoding.UTF8.GetString(buffer, 0, result.Count) }");
+                        await RouteJSONMessageAsync(Encoding.UTF8.GetString(buffer, 0, result.Count));
+
                     }
                     else if (result.MessageType == WebSocketMessageType.Close)
                     {
+                        string id = _manager.GetAllSockets().FirstOrDefault(s => s.Value == webSocket).Key;
                         Console.WriteLine("recieved close message");
-                        Console.WriteLine($"Message: {Encoding.UTF8.GetString(buffer,0,result.Count) }");
+                        _manager.GetAllSockets().TryRemove(id, out WebSocket sock);
+
+                        await sock.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
                         return;
                     }
                 });
@@ -65,6 +72,47 @@ namespace WebSocketServer.Middleware
             {
                 var result = await socket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer), cancellationToken: CancellationToken.None);
                 handleMessage(result, buffer);
+            }
+        }
+
+        public async Task RouteJSONMessageAsync(string message)
+        {
+            var routeOb = JsonConvert.DeserializeObject<dynamic>(message);
+
+            if(Guid.TryParse(routeOb.To.ToString(), out Guid guidOutput))
+            {
+                Console.WriteLine("targeted");
+
+                var sock = _manager.GetAllSockets().FirstOrDefault(s => s.Key == routeOb.To.ToString());
+
+                if(sock.Value != null)
+                {
+                    if(sock.Value.State == WebSocketState.Open)
+                    {
+                        await sock.Value.SendAsync(Encoding.UTF8.GetBytes(routeOb.Message.ToString()),
+                              WebSocketMessageType.Text, true, CancellationToken.None);  
+                    } 
+                }
+                else
+                {
+                    Console.WriteLine("invalid recip ID ");
+                 }
+
+            }
+            else
+            {
+                Console.WriteLine("Broadcast");
+                foreach (var sock in _manager.GetAllSockets())
+                {
+                    if(sock.Value.State == WebSocketState.Open)
+                    {
+                        await sock.Value.SendAsync(Encoding.UTF8.GetBytes(routeOb.Message.ToString()),
+                            WebSocketMessageType.Text, true, CancellationToken.None);
+                    }
+                }
+                {
+
+                }
             }
         }
     }
